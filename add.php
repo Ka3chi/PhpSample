de8b378c488a9364e555e0c129615481c50cdd8e
Ka3chi <aldrinalmoradie69@gmail.com>
2022-11-26 15:23:22 +0800

    update

---
+++ add.php
@@ -0,0 +1,22 @@
+<?php
+$username = $_POST["un"];
+$fullname = $_POST["fullname"];
+$password = $_POST["pass"];
+
+$link = mysqli_connect("localhost", "root", "", "dbsample");
+
+//Check connection
+if ($link === false){
+    die("ERROR: Could not conncet. " . mysqli_connect_error());
+}
+
+//Query
+$sql = "INSERT INTO tbuser SET username='$username', password='$password', fullname='fullname'";
+if (mysli_query($link, $sql)){
+    header("Location: view.php");
+} else {
+    echo "ERROR: Could not able to excute sql. " . mysqli_error($link);
+}
+//Close connection
+mysqli_close($link);
+?>
